edd915f96d892ef696e7ab3b7ddf6228cae0c5cf
Andrew Dudek <andrewdudek84@gmail.com>
2011-09-27 17:54:41 -0500

    Added Files

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+
+gem 'rake', '0.9.2'
+
+# Bundle edge Rails instead:
+# gem 'rails',     :git => 'git://github.com/rails/rails.git'
+
+gem 'json'
+gem 'authlogic'
+gem 'mysql'
+gem 'actionmailer'
+
+gem 'jquery-rails'
+
+# Use unicorn as the web server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# To use debugger
+# gem 'ruby-debug'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QrcodeGame::Application.config.secret_token = '882f0873047e37bda3abce69688ff0eead611a5c87244f6572cf77a3da65b72af71c137e81469ab0ad2009daff4147a3ddaff521f0725872d82e1700b8b7a389'
